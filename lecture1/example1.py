47febb10f4fb7184aaa98541c7e4c8aedf643779
Sama Ahmed <26samahmed@csu.fullerton.edu>
2025-01-24 17:17:17 -0800

    Create example1.py

---
+++ lecture1/example1.py
@@ -0,0 +1,19 @@
+# Create Algirithm to find Max
+# Input: a list of numbers L
+# Output: the maximum number in the list  L
+# maxValue <- L[0]
+  # for each number n in L starting from second element
+  #     if n > maxValue   then
+  #         maxValue <- n
+  # end for
+  # return maxValue
+
+def maxValue(L):
+    maxValue = L[0]
+    for n in L[1:]:
+        if n > maxValue:
+            maxValue = n
+    return maxValue
+
+numbers = [17, 2, 8, 117, 1008]
+print("The maximum value is:", maxValue(numbers))
